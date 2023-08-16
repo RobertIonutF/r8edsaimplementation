5e083455eabe2ea88e97693f662e96a828ca9b93
RobertIonutF <robertionutfundulea@gmail.com>
2023-08-16 21:38:04 +0300

    Added docs for r8edsa.py

--- performance.py
+++ performance.py
@@ -1,72 +1,87 @@
 import time
 import tracemalloc
-import json
 from r8edsa import R8EDSA
+from concurrent.futures import ThreadPoolExecutor
 
-RSA_KEY_LENGTHS = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
-SAMPLE_SIZES = [5, 10, 25, 50, 100]
-MESSAGES = ["t", "te", "tes", "test"]
+RSA_KEY_LENGTHS = [512, 1024, 2048, 4096, 8192]
+SAMPLE_SIZES = [10, 15, 20, 25, 50, 100, 150, 200, 250, 500, 1000, 1500, 2000, 2500, 5000]
+MESSAGES = ["test", "message", "encrypted message", "this is a test message", "quantum123", "robert", "someSecretKey42"]
+totalTests = len(RSA_KEY_LENGTHS) * len(SAMPLE_SIZES) * len(MESSAGES)
+progress = 0
+
+def run_algorithm_test(message, rsa_key_length, sample_size):
+    global progress
+    try:
+        # Measure memory before the algorithm starts
+        tracemalloc.start()
+        start_current, _ = tracemalloc.get_traced_memory()
+        
+        # Algorithm execution
+        start_time = time.time()
+        r8edsa = R8EDSA(message, rsa_key_length, sample_size)
+        r8edsa.execute_r8edsa()
+        end_time = time.time()
+
+        # Measure memory after the algorithm ends
+        end_current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+
+        execution_time = end_time - start_time
+        speed = len(message) / execution_time
+
+        # Calculate the actual memory used by the algorithm
+        actual_memory_used = (end_current - start_current) / 10**6
+
+        progress += 1
+        print(f"Running {progress} tests out of {totalTests}")
+        
+        # Return the result
+        return {
+            "message": f"Testing message length {len(message)}, key length {rsa_key_length} and sample size {sample_size}",
+            "parameters": {
+                "message_length": len(message),
+                "rsa_key_length": rsa_key_length,
+                "bb84_eavesdrop_size": sample_size
+            },
+            "results": {
+                "execution_time": f"{execution_time} s",
+                "speed": f"{speed} chars/s",
+                "memory_before_execution": f"{start_current / 10**6} MB",
+                "memory_after_execution": f"{end_current / 10**6} MB",
+                "actual_memory_used": f"{actual_memory_used} MB",
+                "peak_memory_usage": f"{peak / 10**6} MB"
+            }
+        }
+    except Exception as e:
+        progress += 1
+        print(f"Running {progress} tests out of {totalTests}")
+        return {
+            "message": f"Testing message length {len(message)}, key length {rsa_key_length} and sample size {sample_size}",
+              "parameters": {
+                "message_length": len(message),
+                "rsa_key_length": rsa_key_length,
+                "bb84_eavesdrop_size": sample_size
+            },
+            "results": {
+                "execution_time": "Failed",
+                "speed": "Failed",
+                "memory_before_execution": "Failed",
+                "memory_after_execution": "Failed",
+                "actual_memory_used": "Failed",
+                "peak_memory_usage": "Failed"
+            }
+        }
 
 def generatePerformanceTests(rsa_key_lengths, sample_sizes, messages):
     results = []
 
-    for message in messages:
-        for rsa_key_length in rsa_key_lengths:
-            for sample_size in sample_sizes:
-                print(f"Testing message length {len(message)}, key length {rsa_key_length} and sample size {sample_size}")
-                
-                try:
-                    
-                    # Start tracing memory allocations
-                    tracemalloc.start()
+    with ThreadPoolExecutor(12) as executor:
+        futures = [executor.submit(run_algorithm_test, message, rsa_key_length, sample_size) 
+                   for message in messages 
+                   for rsa_key_length in rsa_key_lengths 
+                   for sample_size in sample_sizes]
 
-                    start_time = time.time()
-                    r8edsa = R8EDSA(message, rsa_key_length, sample_size)
-                    r8edsa.execute_r8edsa()
-                    end_time = time.time()
-
-                    # Stop tracing memory allocations
-                    current, peak = tracemalloc.get_traced_memory()
-                    tracemalloc.stop()
-
-                    execution_time = end_time - start_time
-                    speed = len(message) / execution_time
-
-                    # Add the result to the list
-                    results.append({
-                        "message": f"Testing message length {len(message)}, key length {rsa_key_length} and sample size {sample_size}",
-                        "parameters": {
-                            "message_length": len(message),
-                            "rsa_key_length": rsa_key_length,
-                            "sample_size": sample_size
-                        },
-                        "results": {
-                            "execution_time": execution_time,
-                            "speed": speed,
-                            "current_memory_usage": current / 10**6,
-                            "peak_memory_usage": peak / 10**6
-                        }
-                    })
-                    print("Test completed successfully!")
-                except Exception as e:
-                    results.append({
-                        "message": f"Testing message length {len(message)}, key length {rsa_key_length} and sample size {sample_size}",
-                        "parameters": {
-                            "message_length": len(message),
-                            "rsa_key_length": rsa_key_length,
-                            "sample_size": sample_size
-                        },
-                        "results": {
-                            "execution_time": "Failed",
-                            "speed": "Failed",
-                            "current_memory_usage": "Failed",
-                            "peak_memory_usage": "Failed"
-                        }
-                    })
-                    print("Test failed!")
-                    continue
+        for future in futures:
+            results.append(future.result())
 
     return results
-            
-
-
--- r8edsa.py
+++ r8edsa.py
@@ -7,17 +7,53 @@ import ecdsa
 import time
 
 class R8EDSA:
+    """
+    A class that implements the R8EDSA protocol for quantum cryptography.
+
+    Attributes:
+    - message_str (str): the message to be encrypted and signed
+    - rsa_key_size (int): the size of the RSA key to be used for encryption
+    - sample_size (int): the number of bits to be sampled for error checking
+    - public_key (rsa.PublicKey): the public key used for encryption
+    - private_key (rsa.PrivateKey): the private key used for decryption and signing
+    """
+
     def __init__(self, message_str: str, rsa_key_size: int, sample_size: int):
+        """
+        Initializes a new instance of the R8EDSA class.
+
+        Args:
+        - message_str (str): the message to be encrypted and signed
+        - rsa_key_size (int): the size of the RSA key to be used for encryption
+        - sample_size (int): the number of bits to be sampled for error checking
+        """
         self.message_str = message_str
         self.rsa_key_size = rsa_key_size
         self.sample_size = sample_size
         self.public_key, self.private_key = rsa.newkeys(rsa_key_size)
 
     def prepare_message(self):
+        """
+        Prepares the message for encryption by converting it to binary.
+
+        Returns:
+        - str: the binary representation of the message
+        """
         binary_message = bytes(''.join(format(i, '08b') for i in self.message_str.encode('utf-8')), 'utf-8')
         return ''.join(format(i, '08b') for i in binary_message)
 
     def encryption_process(self, bits, bases, measurement_bases):
+        """
+        Performs the encryption process of the R8EDSA protocol.
+
+        Args:
+        - bits (numpy.ndarray): the binary representation of the message
+        - bases (numpy.ndarray): the bases used for encoding the message
+        - measurement_bases (numpy.ndarray): the bases used for measuring the message
+
+        Returns:
+        - list[int]: the key used for encryption
+        """
         quantum_message = self.encode_message(bits, bases)
         results = self.measure_message(quantum_message, measurement_bases)
         key = self.remove_garbage(bases, measurement_bases, bits)
@@ -29,17 +65,35 @@ class R8EDSA:
         return key
 
     def message_conversion_and_encryption(self, key):
+        """
+        Converts the decrypted message to bytes, encrypts it using RSA, and returns the encrypted message.
+
+        Args:
+        - key (list[int]): the decrypted message
+
+        Returns:
+        - bytes: the encrypted message
+        """
         binary_decrypted_message = ''.join(str(i) for i in key)
         bytes_decrypted_message = int(binary_decrypted_message, 2).to_bytes((int(binary_decrypted_message, 2).bit_length() + 7) // 8, 'big')
         hex_decrypted_message = bytes_decrypted_message.hex()
         return self.encrypt(hex_decrypted_message.encode('utf-8'), self.public_key)
 
     def decryption_and_signature(self, encrypted_message):
+        """
+        Decrypts the encrypted message using RSA, generates a signature using ECDSA, and verifies the signature.
+
+        Args:
+        - encrypted_message (bytes): the encrypted message
+        """
         decrypted_message = self.decrypt(encrypted_message, self.private_key)
         hashed_decrypted_message = hashlib.sha256(decrypted_message).digest()[:32]
         self.generate_signature(hashed_decrypted_message)
 
     def execute_r8edsa(self):
+        """
+        Executes the R8EDSA protocol.
+        """
         start_time = time.time()
         binary_encrypted_message = self.prepare_message()
         n = len(binary_encrypted_message)
@@ -52,9 +106,28 @@ class R8EDSA:
         end_time = time.time()
 
     def generate_bits(self, message):
+        """
+        Generates an array of bits from a binary message.
+
+        Args:
+        - message (str): the binary message
+
+        Returns:
+        - numpy.ndarray: the array of bits
+        """
         return np.array(list(map(int, list(message))))
 
     def encode_message(self, bits, bases):
+        """
+        Encodes the message using quantum circuits.
+
+        Args:
+        - bits (numpy.ndarray): the binary representation of the message
+        - bases (numpy.ndarray): the bases used for encoding the message
+
+        Returns:
+        - list[QuantumCircuit]: the encoded message
+        """
         message = []
         for i in range(len(bits)):
             qc = QuantumCircuit(1,1)
@@ -72,6 +145,16 @@ class R8EDSA:
         return message
 
     def measure_message(self, message, bases):
+        """
+        Measures the encoded message using the specified bases.
+
+        Args:
+        - message (list[QuantumCircuit]): the encoded message
+        - bases (numpy.ndarray): the bases used for measuring the message
+
+        Returns:
+        - list[int]: the measured bits
+        """
         backend = Aer.get_backend('aer_simulator')
         measurements = []
         for q in range(len(bases)):
@@ -86,24 +169,81 @@ class R8EDSA:
         return measurements
 
     def remove_garbage(self, a_bases, b_bases, bits):
+        """
+        Removes the bits that were measured in the wrong bases.
+
+        Args:
+        - a_bases (numpy.ndarray): the bases used for encoding the message
+        - b_bases (numpy.ndarray): the bases used for measuring the message
+        - bits (numpy.ndarray): the binary representation of the message
+
+        Returns:
+        - list[int]: the key used for encryption
+        """
         return [bits[q] for q in range(len(a_bases)) if a_bases[q] == b_bases[q]]
 
     def sample_bits(self, bits, selection):
+        """
+        Samples a specified number of bits from the key.
+
+        Args:
+        - bits (list[int]): the key used for encryption
+        - selection (numpy.ndarray): the indices of the bits to be sampled
+
+        Returns:
+        - list[int]: the sampled bits
+        """
         return [bits.pop(np.mod(i, len(bits))) for i in selection]
 
     def check_eavesdropping(self, sample, results_sample):
+        """
+        Checks if eavesdropping has occurred during the encryption process.
+
+        Args:
+        - sample (list[int]): the sampled key
+        - results_sample (list[int]): the sampled results
+
+        Returns:
+        - str: a message indicating whether eavesdropping was detected or not
+        """
         if sample != results_sample:
             print("Eavesdropping detected!")
         else:
             print("No eavesdropping detected.")
 
     def encrypt(self, message, public_key):
+        """
+        Encrypts a message using RSA.
+
+        Args:
+        - message (bytes): the message to be encrypted
+        - public_key (rsa.PublicKey): the public key used for encryption
+
+        Returns:
+        - bytes: the encrypted message
+        """
         return rsa.encrypt(message, public_key)
 
     def decrypt(self, message, private_key):
+        """
+        Decrypts a message using RSA.
+
+        Args:
+        - message (bytes): the message to be decrypted
+        - private_key (rsa.PrivateKey): the private key used for decryption
+
+        Returns:
+        - bytes: the decrypted message
+        """
         return rsa.decrypt(message, private_key)
 
     def generate_signature(self, hashed_decrypted_message):
+        """
+        Generates a signature using ECDSA and verifies it.
+
+        Args:
+        - hashed_decrypted_message (bytes): the hashed decrypted message
+        """
         sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
         signature = sk.sign(hashed_decrypted_message)
         vk = sk.get_verifying_key()
